9edaaaef1cca5ff60a93a2ce979dafffe7288d1c
Haeseung Lee <dlgomtd93@gmail.com>
2025-01-13 23:58:56 +0900

    init project

---
+++ src/main/java/com/samso/linkjoa/LinkjoaApplication.java
@@ -0,0 +1,13 @@
+package com.samso.linkjoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkjoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkjoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samso/linkjoa/LinkjoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samso.linkjoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkjoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
