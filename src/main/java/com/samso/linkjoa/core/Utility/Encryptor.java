77dcca7c33888139afc5652b60b9ec8f36a5433a
Haeseung Lee <dlgomtd93@gmail.com>
2025-01-28 22:34:34 +0900

    feat : 암복호화

---
+++ src/main/java/com/samso/linkjoa/core/Utility/Encryptor.java
@@ -0,0 +1,34 @@
+package com.samso.linkjoa.core.Utility;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Encryptor {
+
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private static final String AES_ALGORITHM = "AES";
+    private static final String AES_SECRET_KEY = "7254527391128452";
+
+    public static String oneWayEncrypt(String text){
+        return passwordEncoder.encode(text);
+    }
+
+    public static boolean matchOneWay(String plainText, String encryptedText){
+        return passwordEncoder.matches(plainText, encryptedText);
+    }
+
+    public static String twoWayEncrypt(String text){
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(AES_SECRET_KEY.getBytes(), AES_ALGORITHM);
+            Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            byte[] encryptedBytes = cipher.doFinal(text.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Encryption error", e);
+        }
+    }
+}
