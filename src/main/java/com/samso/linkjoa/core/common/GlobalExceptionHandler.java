a8101cdaef1bdea5fba25d929a166d4a0d75d260
Haeseung Lee <dlgomtd93@gmail.com>
2025-01-28 22:34:28 +0900

    feat : Exception 처리

---
+++ src/main/java/com/samso/linkjoa/core/common/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.samso.linkjoa.core.common;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataIntegrityViolationException(Exception e){
+        return new ErrorResponse(e.getMessage());
+    }
+    /*공통처리*/
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleGlobalException(Exception e) {
+        return new ErrorResponse(e.getMessage());
+    }
+}
