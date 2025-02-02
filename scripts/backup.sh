4cfe9dc3655692e4097db1c7054f87355cb5586d
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-03 00:03:38 +0900

    chore : build 파일 수정

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,8 @@ set -e
 
 DEPLOY_PATH="/home/ec2-user/backend/deploy"
 BACKUP_PATH="/home/ec2-user/backend/backup/deploy"
+APPLICATION_PATH="/home/ec2-user/backend/application"
+APP_BACKUP_PATH="/home/ec2-user/backend/backup/application"
 #이전 백업본 삭제
 rm -rf BACKUP_PATH/*
 
@@ -10,3 +12,8 @@ cp -r $DEPLOY_PATH/* $BACKUP_PATH/
 
 #기존 버전 삭제
 rm -rf $DEPLOY_PATH/*
+
+#APPLICATION 이전 백업본 삭제
+rm -rf $APP_BACKUP_PATH/*
+#APPLICATION 백업
+cp $APPLICATION_PATH/* $APP_BACKUP_PATH/*
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ set -e
 DEPLOY_PATH="/home/ec2-user/backend/deploy"
 
 # 배포 파일 권한 변경
-sudo chown ec2-user:ec2-user -R /home/ec2-user/backend/deploy/*
+sudo chown ec2-user:ec2-user -R $DEPLOY_PATH/*
