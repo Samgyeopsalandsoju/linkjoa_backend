ce43897937c4e41a62e37f479f9ee2ad0c2b540f
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-07 02:04:46 +0900

    fix : mailsender 컴포넌트 추가

--- src/main/java/com/samso/linkjoa/domain/mail/MailSender.java
+++ src/main/java/com/samso/linkjoa/domain/mail/MailSender.java
@@ -1,7 +1,10 @@
 package com.samso.linkjoa.domain.mail;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Optional;
 
+@Component
 public interface MailSender {
     public boolean sendMail(String to, String subject, String body);
 
