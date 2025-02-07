f7d720306b089fc4e2385264152e88e91a74a6d8
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-08 05:07:33 +0900

    fix : 공통 response 처리 string, 원싵 타입도 되도록 수정

--- src/main/java/com/samso/linkjoa/core/common/ResponseWrapper.java
+++ src/main/java/com/samso/linkjoa/core/common/ResponseWrapper.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
-public class reponseWrapper implements ResponseBodyAdvice<Object> {
+public class ResponseWrapper implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
--- src/main/java/com/samso/linkjoa/core/config/CommonHttpMessageConverter.java
+++ src/main/java/com/samso/linkjoa/core/config/CommonHttpMessageConverter.java
@@ -6,12 +6,17 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)//TODO 뭐지?
@@ -24,21 +29,27 @@ public class CommonHttpMessageConverter extends AbstractHttpMessageConverter<Api
     }
 
     @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return Collections.singletonList(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
     protected boolean supports(Class<?> clazz) {
 
-        //return clazz.equals(ApiResponse.class) || clazz.isPrimitive() || clazz.equals(String.class);
         return clazz.isPrimitive() || clazz.equals(String.class);
     }
 
     @Override
     protected ApiResponse<Object> readInternal(Class<? extends ApiResponse<Object>> clazz, HttpInputMessage inputMessage)
             throws IOException, HttpMessageNotReadableException {
-        return objectMapper.readValue(inputMessage.getBody(), clazz);
+       // return objectMapper.readValue(inputMessage.getBody(), clazz);
+        throw new UnsupportedOperationException("This converter can only support writing operation.");
     }
 
     @Override
     protected void writeInternal(ApiResponse<Object> resultMessage, HttpOutputMessage outputMessage)
             throws IOException, HttpMessageNotWritableException {
         String responseMessage = this.objectMapper.writeValueAsString(resultMessage);
+        StreamUtils.copy(responseMessage.getBytes(StandardCharsets.UTF_8), outputMessage.getBody());
     }
 }
