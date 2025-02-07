91f618cde937a6dcbe18044bbe7fa9ce677bdc88
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-08 05:06:34 +0900

    fix : 로그인 처리 수정

--- src/main/java/com/samso/linkjoa/application/member/LoginUseCase.java
+++ src/main/java/com/samso/linkjoa/application/member/LoginUseCase.java
@@ -1,15 +1,21 @@
 package com.samso.linkjoa.application.member;
 
 import com.samso.linkjoa.core.Utility.Encryptor;
+import com.samso.linkjoa.domain.member.Member;
 import com.samso.linkjoa.domain.member.MemberEnum;
 import com.samso.linkjoa.domain.member.MemberRepository;
 import com.samso.linkjoa.presentation.member.request.LoginRequest;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -19,12 +25,16 @@ public class LoginUseCase {
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public String login(HttpServletRequest request, LoginRequest loginRequest) {
 
-        String mail = loginRequest.getMail();
+        String mail = Encryptor.twoWayEncrypt(loginRequest.getMail());
         String password = loginRequest.getPassword();
 
-        return memberRepository.findByMail(mail)
-                .map(member -> Encryptor.matchOneWay(password, member.getPassword()) ? MemberEnum.LOGIN_SUCCESS.getValue() : MemberEnum.PASSWORD_MISMATCH.getValue())
-                .orElseThrow(() -> new AuthenticationException(MemberEnum.NOT_JOINED_USER.name()) {
-                });
+        Member member = memberRepository.findByMail(mail)
+                .orElseThrow(() -> new EntityNotFoundException(MemberEnum.NOT_JOINED_USER.getValue()));
+
+        Optional.of(member)
+                .filter(m -> Encryptor.matchOneWay(password, m.getPassword()))
+                .orElseThrow(() -> new IllegalArgumentException(MemberEnum.PASSWORD_MISMATCH.getValue()));
+
+        return MemberEnum.LOGIN_SUCCESS.getValue();
     }
 }
--- src/main/java/com/samso/linkjoa/domain/member/MemberEnum.java
+++ src/main/java/com/samso/linkjoa/domain/member/MemberEnum.java
@@ -11,8 +11,9 @@ public enum MemberEnum {
     LOGIN_SUCCESS("1001"),   //로그인 성공
     NOT_EXIST_VERIFY_INFO("1002"), //메일 인증 이력 없음
     DIFFERENT_MAIL_OF_VERIFIED_MAIL("1003"),//인증된 메일과 회원가입 시도한 메일이 불일치
-    NOT_JOINED_USER("1004"),//회원정보 없음
-    PASSWORD_MISMATCH("1005");//비밀번호 불일치
+    ALREADY_JOINED_USER("1004"),
+    NOT_JOINED_USER("1005"),//회원정보 없음
+    PASSWORD_MISMATCH("1006");//비밀번호 불일치
 
     private final String value;
 }
