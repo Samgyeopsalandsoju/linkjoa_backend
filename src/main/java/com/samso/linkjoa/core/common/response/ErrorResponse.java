72e1bd0935a3d509e9c3ff9d09d3a92f5b39801b
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-10 00:09:45 +0900

    fix : 변수명 수정

--- src/main/java/com/samso/linkjoa/core/common/response/ErrorResponse.java
+++ src/main/java/com/samso/linkjoa/core/common/response/ErrorResponse.java
@@ -1,18 +1,15 @@
 package com.samso.linkjoa.core.common.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
 
 @Getter
 public class ErrorResponse {
     private final String code;
-    private final HttpStatus status;
+    private final HttpStatus httpStatus;
 
     public ErrorResponse(String code, HttpStatus status){
         this.code = code;
-        this.status = status;
+        this.httpStatus = status;
     }
 }
