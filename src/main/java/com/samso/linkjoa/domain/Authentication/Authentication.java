658f97d816af6bdfb199c7ad0cd2ef9ba6d58889
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-11 00:41:40 +0900

    fix : 인증 key 생성 방식 수정

--- src/main/java/com/samso/linkjoa/domain/Authentication/Authentication.java
+++ src/main/java/com/samso/linkjoa/domain/Authentication/Authentication.java
@@ -3,9 +3,9 @@ package com.samso.linkjoa.domain.Authentication;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
@@ -14,15 +14,18 @@ import java.util.concurrent.ThreadLocalRandom;
 @Getter
 public class Authentication {
 
+    private String authKey;
     private String mail;
     private long authCode;
 
     public Authentication generateAuthCode(String mail){
+
+        String authKey = UUID.randomUUID().toString();
         long generateCode = ThreadLocalRandom.current().nextInt(100000, 1000000);
         System.out.println("mail : " + mail);
         System.out.println("code : " + generateCode);
 
-        return new Authentication(mail, generateCode);
+        return new Authentication(authKey, mail, generateCode);
 
     }
 }
