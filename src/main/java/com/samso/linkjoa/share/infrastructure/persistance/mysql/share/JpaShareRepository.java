1644f9ca1fdcc1acb7893fbf6c6cc69cd070d05d
Haeseung Lee <dlgomtd93@gmail.com>
2025-02-23 22:56:34 +0900

    fix : 어노테이션 추가 및 파일 삭제

--- src/main/java/com/samso/linkjoa/share/infrastructure/persistance/mysql/share/JpaShareRepository.java
+++ src/main/java/com/samso/linkjoa/share/infrastructure/persistance/mysql/share/JpaShareRepository.java
@@ -3,9 +3,11 @@ package com.samso.linkjoa.share.infrastructure.persistance.mysql.share;
 import com.samso.linkjoa.share.domain.entity.Share;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface JpaShareRepository extends JpaRepository<Share, Long> {
     List<Share> findByMemberId(long memberId, Sort sort);
 
